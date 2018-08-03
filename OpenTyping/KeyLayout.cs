35d15e5b47f79711a30885c4d937fa45c98704ca
g34r <g34red@gmail.com>
2018-08-03 14:58:46 +0900

    Add key layout data validation checking

--- OpenTyping/KeyLayout.cs
+++ OpenTyping/KeyLayout.cs
@@ -37,7 +37,33 @@ namespace OpenTyping
 
         public static KeyLayout ParseKeyLayoutData(string data)
         {
-            return JsonConvert.DeserializeObject<KeyLayout>(data);
+            KeyLayout keyLayout = JsonConvert.DeserializeObject<KeyLayout>(data);
+
+            if (string.IsNullOrEmpty(keyLayout.Name))
+            {
+                string message = "자판 데이터의 이름(Name 필드)이 주어지지 않았습니다.";
+                throw new InvalidKeyLayoutDataException(message);
+            }
+
+            var rowNumberData = new List<Tuple<string, int>>
+            {
+                Tuple.Create("숫자열", 13),
+                Tuple.Create("첫째 열", 13),
+                Tuple.Create("둘째 열", 11),
+                Tuple.Create("셋째 열", 10)
+            };
+
+            for (int i = 0; i < keyLayout.KeyLayoutData.Count; i++)
+            {
+                if (keyLayout.KeyLayoutData[i].Count != rowNumberData[i].Item2)
+                {
+                    string message = rowNumberData[i].Item1 + "의 키 개수는 " + rowNumberData[i].Item2 + " 이어야 하는데 "
+                                   + keyLayout.KeyLayoutData[i].Count + "개가 주어졌습니다.";
+                    throw new InvalidKeyLayoutDataException(message);
+                }
+            }
+
+            return keyLayout;
         }
 
         public static KeyLayout LoadKeyLayout(string dataFileLocation)
