84328a355b8d5318ee63e05ec6cd0764f27718fb
g34r <g34red@gmail.com>
2018-08-11 23:58:40 +0900

    Add PracticeData class
    PracticeData class stores text data of a sentence practice and an article practice.

---
+++ OpenTyping/PracticeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTyping
+{
+    class PracticeData
+    {
+        public PracticeData(IList<string> textData, string character)
+        {
+            TextData = textData;
+            Character = character;
+        }
+
+        public IList<string> TextData { get; }
+        public string Character { get; }
+    }
+}
