b84a8bf85825a0be29d6bec57a9fd3f339f9de29
g34r <g34red@gmail.com>
2018-08-14 03:25:56 +0900

    Add SentencePracticeWindow and rename controls namespace to mah

---
+++ OpenTyping/SentencePracticeWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace OpenTyping
+{
+    /// <summary>
+    /// SentencePracticeWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SentencePracticeWindow : MetroWindow
+    {
+        public SentencePracticeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
