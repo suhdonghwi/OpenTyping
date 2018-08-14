d14201d0b92bc1212bbd6b95fcd06ffc72c1eb96
g34r <g34red@gmail.com>
2018-08-14 23:11:01 +0900

    Add Author information to practice data ListBox in Settings

--- OpenTyping/NullToVisibilityConverter.cs
+++ OpenTyping/NullToVisibilityConverter.cs
@@ -9,7 +9,7 @@ namespace OpenTyping
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? Visibility.Hidden : Visibility.Visible;
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
