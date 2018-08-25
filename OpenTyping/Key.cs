02604db28504459918a6090e8acff8c870cc3f7e
g34r <g34red@gmail.com>
2018-08-26 01:56:37 +0900

    Remove unnecessary constructor definitions

--- OpenTyping/Key.cs
+++ OpenTyping/Key.cs
@@ -4,13 +4,5 @@
     {
         public string KeyData { get; set; }
         public string ShiftKeyData { get; set; }
-
-        public Key() {}
-
-        public Key(string keyData, string shiftKeyData = null)
-        {
-            KeyData = keyData;
-            ShiftKeyData = shiftKeyData;
-        }
     }
 }
