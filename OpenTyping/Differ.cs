e72df19bcfebedb18339a575c17afec8b515a4dd
g34r <g34red@gmail.com>
2018-08-27 23:47:53 +0900

    Add highlighting equal and unequal texts

---
+++ OpenTyping/Differ.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTyping
+{
+    class Differ
+    {
+        public class DiffData
+        {
+            public enum DiffState
+            {
+                Equal,
+                Unequal,
+            }
+
+            public string Text { get; }
+            public DiffState State { get; }
+
+            public DiffData(string text, DiffState state)
+            {
+                Text = text;
+                State = state;
+            }
+        }
+
+        public IEnumerable<DiffData> Diff(string text1, string text2)
+        {
+            if (string.IsNullOrEmpty(text1) || string.IsNullOrEmpty(text2))
+            {
+                return new List<DiffData>();
+            }
+
+            int length = Math.Min(text1.Length, text2.Length);
+            string temp = "";
+            bool equalState = text1[0] == text2[0];
+            var result = new List<DiffData>();
+            int i = 0;
+            for (; i < length; i++)
+            {
+                if (text1[i] == text2[i] == equalState)
+                {
+                    temp += text1[i];
+                }
+                else
+                {
+                    result.Add(new DiffData(temp, equalState ? DiffData.DiffState.Equal : DiffData.DiffState.Unequal));
+                    temp = text1[i].ToString();
+                    equalState = !equalState;
+                }
+            }
+
+            result.Add(new DiffData(temp, equalState ? DiffData.DiffState.Equal : DiffData.DiffState.Unequal));
+            if (text1.Length == text2.Length) return result;
+
+            result.Add(new DiffData((text1.Length < text2.Length ? text2 : text1).Substring(i),
+                                    DiffData.DiffState.Unequal));
+            return result;
+        }
+    }
+}
--- OpenTyping/SentencePracticeWindow.xaml.cs
+++ OpenTyping/SentencePracticeWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
 using MahApps.Metro.Controls;
@@ -19,27 +17,7 @@ namespace OpenTyping
         public string CurrentText
         {
             get => currentText;
-            set
-            {
-                currentText = value;
-                currentWordSequence = new List<string>(currentText.Split(' '));
-
-                CurrentTextBlock.Inlines.Clear();
-                foreach (char ch in value)
-                {
-                    CurrentTextBlock.Inlines.Add(new Run(ch.ToString()));
-                }
-            }
-        }
-
-        private List<string> currentWordSequence = new List<string>();
-        private readonly List<string> currentInputHistory = new List<string>();
-
-        private string currentInput = "";
-        public string CurrentInput
-        {
-            get => currentInput;
-            set => SetField(ref currentInput, value);
+            set => SetField(ref currentText, value);
         }
 
         private readonly PracticeData practiceData;
@@ -54,13 +32,11 @@ namespace OpenTyping
             private set => SetField(ref typingSpeed, value);
         }
 
-        private int currentWordIndex = -1;
         private int? currentSentenceIndex;
         private static readonly Random SentenceIndexRandom = new Random();
 
-        private readonly Brush currentWordBack = Brushes.LightGreen;
-        private readonly Brush correctForeground = Brushes.Green;
-        private readonly Brush incorrectForeground = Brushes.Red;
+        private readonly Brush correctForeground = Brushes.LightGreen;
+        private readonly Brush incorrectForeground = Brushes.Pink;
 
         public SentencePracticeWindow(PracticeData practiceData, bool shuffle)
         {
@@ -76,9 +52,9 @@ namespace OpenTyping
 
         private void NextSentence()
         {
-            if (currentSentenceIndex != null)
+            if (CurrentTextBox.Text != "")
             {
-                string previousSentence = practiceData.TextData[currentSentenceIndex.Value];
+                string previousSentence = CurrentTextBox.Text;
                 TypingSpeed = Convert.ToInt32(typingMeasurer.Finish(previousSentence));
             }
 
@@ -117,134 +93,54 @@ namespace OpenTyping
 
             string nextSentence = practiceData.TextData[currentSentenceIndex.Value];
             CurrentText = nextSentence;
-
-            currentWordIndex = -1;
-            NextWord();
-
-            currentInputHistory.Clear();
         }
 
-        private void PreviousWord()
+        private void HighlightDiff()
         {
-            if (currentWordIndex <= 0) return;
+            string input = CurrentTextBox.Text;
 
-            currentWordIndex -= 2;
-            CurrentInput = currentInputHistory[currentWordIndex + 1];
-            CurrentTextBox.CaretIndex = CurrentInput.Length;
-            currentInputHistory.RemoveAt(currentInputHistory.Count - 1);
-
-            NextWord();
-        }
-
-        private void NextWord()
-        {
-            currentWordIndex++;
+            var differ = new Differ();
+            IEnumerable<Differ.DiffData> diffs = differ.Diff(CurrentText.Substring(0, input.Length), CurrentTextBox.Text);
 
             CurrentTextBlock.Inlines.Clear();
-            for (int i = 0; i < currentWordSequence.Count; i++)
+            foreach (Differ.DiffData diff in diffs)
             {
-                string targetWord = currentWordSequence[i];
-
-                if (i == currentWordIndex)
+                var run = new Run(diff.Text);
+                switch (diff.State)
                 {
-                    var newRun = new Run(targetWord)
-                    {
-                        Background = currentWordBack
-                    };
-
-                    CurrentTextBlock.Inlines.Add(newRun);
-                }
-                else if (i < currentWordIndex)
-                {
-                    string inputWord = currentInputHistory[i];
-                    int length = Math.Min(inputWord.Length, targetWord.Length);
-
-                    string tempString = "";
-                    bool correct = inputWord[0] == targetWord[0];
-                    int j = 0;
-                    for (; j < length; j++)
-                    {
-                        char inputChar = inputWord[j];
-                        if ((inputChar == targetWord[j]) == correct)
-                        {
-                            tempString += inputChar;
-                        }
-                        else
-                        {
-                            CurrentTextBlock.Inlines.Add(new Run(tempString)
-                            {
-                                Foreground = correct ? correctForeground : incorrectForeground
-                            });
-                            correct = !correct;
-                            tempString = inputChar.ToString();
-                        }
-                    }
-
-                    if (tempString != "")
-                    {
-                        CurrentTextBlock.Inlines.Add(new Run(tempString)
-                        {
-                            Foreground = correct ? correctForeground : incorrectForeground
-                        });
-                    }
-
-                    if (j < inputWord.Length)
-                    {
-                        CurrentTextBlock.Inlines.Add(new Run(inputWord.Substring(j))
-                        {
-                            Foreground = incorrectForeground,
-                            TextDecorations = TextDecorations.Underline
-                        });
-                    }
-                    else if (j < targetWord.Length)
-                    {
-                        CurrentTextBlock.Inlines.Add(new Run(targetWord.Substring(j))
-                        {
-                            Foreground = incorrectForeground,
-                            TextDecorations = TextDecorations.Strikethrough
-                        });
-                    }
-                }
-                else
-                {
-                    var newRun = new Run(currentWordSequence[i]);
-                    CurrentTextBlock.Inlines.Add(newRun);
+                    case Differ.DiffData.DiffState.Equal:
+                        run.Background = correctForeground;
+                        break;
+                    case Differ.DiffData.DiffState.Unequal:
+                        run.Background = incorrectForeground;
+                        break;
                 }
 
-               CurrentTextBlock.Inlines.Add(new Run(" "));
+                CurrentTextBlock.Inlines.Add(run);
             }
 
-            if (currentWordIndex == currentWordSequence.Count)
-            {
-                NextSentence();
-            }
+            CurrentTextBlock.Inlines.Add(new Run(CurrentText.Substring(input.Length)));
         }
 
         private void CurrentTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Space)
+            if (e.Key == System.Windows.Input.Key.Enter)
             {
-                if (!string.IsNullOrEmpty(CurrentInput))
-                {
-                    currentInputHistory.Add(CurrentInput);
-                    NextWord();
-                    CurrentInput = "";
-                }
-
+                NextSentence();
+                CurrentTextBox.Text = "";
                 e.Handled = true;
+
+                return;
             }
-            else if (e.Key == System.Windows.Input.Key.Back || e.ImeProcessedKey == System.Windows.Input.Key.Back)
-            {
-                if (CurrentTextBox.Text == "")
-                {
-                    PreviousWord();
-                    e.Handled = true;
-                }
-            }
-            else if (!currentInputHistory.Any() && CurrentInput == "")
+            if (CurrentTextBox.Text == "")
             {
                 typingMeasurer.Start();
             }
+        }
+
+        private void CurrentTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            HighlightDiff();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
