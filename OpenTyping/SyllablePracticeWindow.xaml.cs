ef3bb3c9519e763299d73ac327bc612ba05fb267
g34r <g34red@gmail.com>
2018-10-17 21:12:48 +0800

    Add SyllablePracticeWindow

---
+++ OpenTyping/SyllablePracticeWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MahApps.Metro.Controls;
+
+namespace OpenTyping
+{
+    /// <summary>
+    /// SyllablePracticeWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SyllablePracticeWindow : MetroWindow
+    {
+        private readonly string syllablesList;
+
+        public SyllablePracticeWindow(string syllablesList)
+        {
+            InitializeComponent();
+
+            this.syllablesList = syllablesList;
+        }
+    }
+}
