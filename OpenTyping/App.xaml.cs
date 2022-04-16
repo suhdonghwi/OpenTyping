74c8d7c558cf7f433170172eaf0d796d30dac94c
JJ Lee <comseong@gmail.com>
2022-04-16 15:45:02 +0500

    Add leaving logs of unhandled error

--- OpenTyping/App.xaml.cs
+++ OpenTyping/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using NLog;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace OpenTyping
 {
@@ -7,5 +9,20 @@ namespace OpenTyping
     /// </summary>
     public partial class App : Application
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender,
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            var exception = e.Exception;          // get exception
+            logger.Error(exception.ToString()); ; // leave the log to file
+            e.Handled = true;                     // prevent the application from crashing
+            Shutdown();                           // quit the application in a controlled way
+        }
     }
 }
